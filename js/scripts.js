576b8d0878789b0a57c23deb4de7ac0b5a628efd
Lukasz Sliczner <lukaszsliczner@MacBook-Pro.local>
2017-05-31 21:01:27 +0000

    task with drawing the tree solved

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,16 @@
+var height = 5; 
+
 function drawTree(height) {
-	
+	for(var i = 0; i < height; i++) {
+
+		var star = '*';
+
+		for(var j = 0; j < i; j++) {
+			star += '*';
+		}
+
+		console.log(star);
+	}
 }
+
+drawTree(height);
